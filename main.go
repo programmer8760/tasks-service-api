3f824fab848f5828d05ce6fa0c9c0c5a2b09e83d
programmer8760 <programmer8760@gmail.com>
2026-01-21 21:50:33 +0300

    feat: add db connection

--- main.go
+++ main.go
@@ -1,17 +1,27 @@
 package main
 
 import (
-  "log"
+	"log"
 
-  "github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	"github.com/programmer8760/tasks-service-api/db"
+	"github.com/programmer8760/tasks-service-api/types"
 )
 
 func main() {
-  app := fiber.New()
+	godotenv.Load()
 
-  app.Get("/", func (c *fiber.Ctx) error {
-      return c.SendString("200")
-    })
+	database, err := db.Connect()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("DB connected: ", database != nil)
 
-  log.Fatal(app.Listen(":3000"))
+	app := fiber.New()
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("200")
+	})
+
+	log.Fatal(app.Listen(":3000"))
 }
