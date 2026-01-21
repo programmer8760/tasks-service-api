b9bde1e413523403e1defaa55f64cf30de24a645
programmer8760 <programmer8760@gmail.com>
2026-01-21 21:52:03 +0300

    chore: add types declaration

---
+++ types/types.go
@@ -0,0 +1,35 @@
+package types
+
+import "time"
+
+type User struct {
+	id       uint   `gorm:"primaryKey"`
+	login    string `gorm:"unique"`
+	password string
+}
+
+type Status string
+
+const (
+	New        Status = "new"
+	InProgress Status = "in progress"
+	Done       Status = "done"
+)
+
+type Task struct {
+	id          uint `gorm:"primaryKey"`
+	title       string
+	description string
+	status      Status `gorm:"type:varchar(11);not null"`
+	created     time.Time
+	updated     time.Time
+	user_id     uint
+}
+
+type Event struct {
+	id      uint `gorm:"primaryKey"`
+	title   string
+	start   time.Time
+	end     time.Time
+	task_id uint
+}
