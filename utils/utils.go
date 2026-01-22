9887a475eb023606485ce293b4d254e7822d95b4
programmer8760 <programmer8760@gmail.com>
2026-01-23 00:43:25 +0300

    chore: add CheckJWT util func

--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,10 @@
 package utils
 
 import (
+	"errors"
+	"os"
+
+	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -20,4 +24,21 @@ func CheckPassword(passwordHash string, attempt string) error {
 		[]byte(passwordHash),
 		[]byte(attempt),
 	)
+}
+
+func CheckJWT(tokenString string) (map[string]any, error) {
+	secret := []byte(os.Getenv("JWT_SECRET"))
+
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (any, error) {
+		return secret, nil
+	}, jwt.WithValidMethods([]string{jwt.SigningMethodHS256.Alg()}))
+	if err != nil {
+		return nil, err
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok {
+		return claims, nil
+	} else {
+		return nil, errors.New("invalid claims type")
+	}
 }
