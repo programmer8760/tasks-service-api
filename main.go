7c478d42f87e0608112fcee8dadc10af24990731
programmer8760 <programmer8760@gmail.com>
2026-01-22 19:03:22 +0300

    fix: update models package import

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
 	"github.com/programmer8760/tasks-service-api/db"
-	"github.com/programmer8760/tasks-service-api/types"
+	"github.com/programmer8760/tasks-service-api/models"
 )
 
 func main() {
@@ -19,12 +19,13 @@ func main() {
 	log.Println("DB connected: ", database != nil)
 
 	database.AutoMigrate(
-		&types.User{},
-		&types.Task{},
-		&types.Event{},
+		&models.User{},
+		&models.Task{},
+		&models.Event{},
 	)
 
 	app := fiber.New()
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("200")
 	})
