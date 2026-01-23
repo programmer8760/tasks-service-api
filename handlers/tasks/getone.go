cca89b99d0322530294e96c71928df6002263cfc
programmer8760 <programmer8760@gmail.com>
2026-01-23 23:02:49 +0300

    chore: add get one task endpoint handler

---
+++ handlers/tasks/getone.go
@@ -0,0 +1,54 @@
+package tasks
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/programmer8760/tasks-service-api/models"
+	"github.com/programmer8760/tasks-service-api/utils"
+	"gorm.io/gorm"
+)
+
+type GetOneTaskRequest struct {
+	Token string `json:"token"`
+}
+
+func GetOneTask(db *gorm.DB) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		var request GetOneTaskRequest
+
+		taskID := c.Params("taskID")
+
+		err := c.BodyParser(&request)
+		if err != nil {
+			return fiber.ErrBadRequest
+		}
+
+		tokenClaims, err := utils.CheckJWT(request.Token)
+		if err != nil {
+			return fiber.ErrUnauthorized
+		}
+		userID, err := utils.GetUserIDFromJWTClaims(tokenClaims)
+		if err != nil {
+			return fiber.ErrUnauthorized
+		}
+
+		var task models.Task
+		result := db.
+			Where("id = ? AND user_id = ?", taskID, userID).
+			Find(&task)
+
+		if result.Error != nil {
+			log.Println("error getting data from db: ", result.Error)
+			return fiber.ErrInternalServerError
+		}
+		if result.RowsAffected == 0 {
+			return fiber.ErrNotFound
+		}
+
+		return c.JSON(fiber.Map{
+			"status": 200,
+			"task":   task,
+		})
+	}
+}
