9d70a7fdaa522164d0d9440ee487eb486d47aefa
programmer8760 <programmer8760@gmail.com>
2026-01-23 21:34:39 +0300

    chore: add get all tasks endpoint handler

---
+++ handlers/tasks/getall.go
@@ -0,0 +1,40 @@
+package tasks
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/programmer8760/tasks-service-api/models"
+	"github.com/programmer8760/tasks-service-api/utils"
+	"gorm.io/gorm"
+)
+
+type GetAllTasksRequest struct {
+	Token string `json:"token"`
+}
+
+func GetAllTasks(db *gorm.DB) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		var request GetAllTasksRequest
+
+		err := c.BodyParser(&request)
+		if err != nil {
+			return fiber.ErrBadRequest
+		}
+
+		tokenClaims, err := utils.CheckJWT(request.Token)
+		if err != nil {
+			return fiber.ErrUnauthorized
+		}
+
+		var tasks []models.Task
+
+		db.Find(
+			&tasks,
+			models.Task{UserID: uint(tokenClaims["user_id"].(float64))},
+		)
+
+		return c.JSON(fiber.Map{
+			"status": 200,
+			"tasks":  tasks,
+		})
+	}
+}
